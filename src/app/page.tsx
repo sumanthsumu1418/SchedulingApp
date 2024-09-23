17b2124b2b39ff73d91076451c1fe5a9f0265a79
sumanth <67574368+sumanthsumu1418@users.noreply.github.com>
2024-09-23 13:44:34 +0530

    update

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,10 +2,16 @@ import Image from "next/image";
 
 export default function Home() {
   return (
+    <>
     <div className="flex flex-col justify-center items-center">
       <h1 className="text-[2rem] font-bold ">IGNITED MINDS</h1>
       <h1 className="text-[1.3rem] font-semibold">Scheduling App</h1>
       <p className="text-[0.9rem] font-normal">For Job Aspirants</p>
+      
     </div>
+    <div className="flex flex-col justify-center mt-[12%] text-center text-[#CB1517] text-[3rem] font-bold">
+        coming soon......
+    </div>
+    </>
   );
 }
